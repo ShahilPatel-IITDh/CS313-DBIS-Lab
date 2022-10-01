43e7d7640962caa00675f0de9a6620dee8f6a776
Shahil Patel <83348636+shahilpatel-3602@users.noreply.github.com>
2022-10-01 17:08:46 +0530

    Working index.html and StudentServlet.java for Part-B

--- Assignment-5/Part-B supporting file/StudentServlet.java
+++ Assignment-5/Part-B supporting file/StudentServlet.java
@@ -20,30 +20,30 @@ public class StudentServlet extends HttpServlet{
 
 		response.setContentType("text/html;charset=UTF-8");
 		
-		String student_id = request.getParameter("id");//getting student id as input from index.html page
+		String advisor_ID = request.getParameter("id");//getting student id as input from index.html page
 		
 		PrintWriter out = response.getWriter();
 		Connection conn = null;
 		Statement  stmt = null;
-		String student_name=null;
 		String dept_name=null;
+//		String dept_name=null;
 		try {
 			out.println("<!DOCTYPE html>");//print in the form of HTML code
 			out.println("<html>");
 			out.println("<head><title>Advisor Query Servlet</title></head>");
 			out.println("<body>");
 			Class.forName("com.mysql.jdbc.Driver"); //loading mysql driver
-			String query="select * from student where id=?	"; //query to get the student details with id 
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/university", "pavan", "12345678");//mysql connection with username and password
+			String query="SELECT ID, dept_name FROM advisor, instructor WHERE advisor.i_ID = instructor.ID AND id=?	"; //query to get the student details with id 
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/university", "universityDB0039", "Password");//mysql connection with username and password
 			PreparedStatement ps = conn.prepareStatement(query);
-			ps.setString(1, student_id);
+			ps.setString(1, advisor_ID);
 			ResultSet rset = ps.executeQuery();
 			int count=0;
 			while(rset.next()) {
-				student_name=rset.getString("name");//getting student name and storing in a variable
+				dept_name = rset.getString("dept_name");//getting student name and storing in a variable
 				++count;
 			}
-			out.println("Student id is " +student_id+" Student name is "+student_name);//printing student id and name
+			out.println("Advisor ID is " +advisor_ID+" department name is "+dept_name);//printing student id and name
 			out.println("<p>==== " + count + " rows found =====</p>");
 			out.println("</body></html>");
 			
